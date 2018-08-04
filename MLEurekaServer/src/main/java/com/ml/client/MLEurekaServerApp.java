9c6a8efbbc4b82efb6c54fbb412f9a81ce3ef93f
Cheeran Santhosh <cheeran@hotmail.com>
2018-08-03 20:27:59 -0400

    New Version

---
+++ MLEurekaServer/src/main/java/com/ml/client/MLEurekaServerApp.java
@@ -0,0 +1,17 @@
+package com.ml.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MLEurekaServerApp {
+
+	// This object is the eureka server registry maintainer app
+    public static void main(String[] args) {
+    	
+    	System.setProperty("spring.config.name", "MLEurekaServer");
+        SpringApplication.run(MLEurekaServerApp.class, args);        
+    }
+}
