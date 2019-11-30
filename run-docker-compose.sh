693eb6de9c8ff398c03be17bf3b8fa47202911a3
Santhosh Cheeran <40557059+cheerans@users.noreply.github.com>
2019-11-30 04:42:51 -0500

    Update run-docker-compose.sh

--- run-docker-compose.sh
+++ run-docker-compose.sh
@@ -1,4 +1,4 @@
-docker-compose -f $PWD\\docker-compose-east-framework.yml up --force-recreate app
-docker-compose -f $PWD\\docker-compose-east.yml up --force-recreate app
-#docker-compose -f $PWD\\docker-compose-west-framework.yml up  --force-recreate app
-#docker-compose -f $PWD\\docker-compose-west.yml up --force-recreate app
+docker-compose -f $PWD//docker-compose-east-framework.yml up --force-recreate app
+docker-compose -f $PWD//docker-compose-east.yml up --force-recreate app
+#docker-compose -f $PWD//docker-compose-west-framework.yml up  --force-recreate app
+#docker-compose -f $PWD//docker-compose-west.yml up --force-recreate app
