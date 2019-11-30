58e6fd88dbe710c974fc62292d4cce441632a716
Santhosh Cheeran <40557059+cheerans@users.noreply.github.com>
2019-11-30 06:08:17 -0500

    Update run-docker-compose.sh

--- run-docker-compose.sh
+++ run-docker-compose.sh
@@ -1,4 +1,8 @@
+docker network create ml-cloud-network-east
+docker volume create mlservicedatadir-east
 docker-compose -f "$PWD//docker-compose-east-framework.yml" up msapigateway-east-server1
 docker-compose -f "$PWD//docker-compose-east.yml" up mlserver-east
+#docker network create ml-cloud-network-west
+#docker volume create mlservicedatadir-west
 #docker-compose -f "$PWD//docker-compose-west-framework.yml" up msapigateway-west-server1
 #docker-compose -f "$PWD//docker-compose-west.yml" up mlserver-west
