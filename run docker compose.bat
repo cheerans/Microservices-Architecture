8ebb76c7060951d0fe501502dc694bca289239ed
Santhosh Cheeran <40557059+cheerans@users.noreply.github.com>
2019-11-30 04:34:37 -0500

    Update and rename run docker compose.bat to run-docker-compose.sh

--- run docker compose.bat
+++
@@ -1,4 +0,0 @@
-docker-compose -f $PWD\docker-compose-east-framework.yml --force-recreate app
-docker-compose -f $PWD\docker-compose-east.yml --force-recreate app
-#docker-compose -f $PWD\docker-compose-west-framework.yml --force-recreate app
-#docker-compose -f $PWD\docker-compose-west.yml --force-recreate app
