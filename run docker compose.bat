2a5ad1771a7408c4838b1d48a66d75ca14781265
Santhosh Cheeran <40557059+cheerans@users.noreply.github.com>
2019-11-30 04:30:09 -0500

    Update run docker compose.bat

--- run docker compose.bat
+++ run docker compose.bat
@@ -1,4 +1,4 @@
-docker-compose up -f $PWD\docker-compose-east-framework.yml
-docker-compose up -f $PWD\docker-compose-east.yml
-#docker-compose up -f $PWD\docker-compose-west-framework.yml
-#docker-compose up -f $PWD\docker-compose-west.yml
+docker-compose -f $PWD\docker-compose-east-framework.yml --force-recreate app
+docker-compose -f $PWD\docker-compose-east.yml --force-recreate app
+#docker-compose -f $PWD\docker-compose-west-framework.yml --force-recreate app
+#docker-compose -f $PWD\docker-compose-west.yml --force-recreate app
