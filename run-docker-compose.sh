0273aa058cfadaabe33dd1ae98f34d66780f4470
Santhosh Cheeran <40557059+cheerans@users.noreply.github.com>
2019-11-30 05:02:18 -0500

    Update run-docker-compose.sh

--- run-docker-compose.sh
+++ run-docker-compose.sh
@@ -1,3 +1,4 @@
+docker volume create --name=mlservicedatadir
 docker-compose -f $PWD//docker-compose-east-framework.yml up --force-recreate app
 docker-compose -f $PWD//docker-compose-east.yml up --force-recreate app
 #docker-compose -f $PWD//docker-compose-west-framework.yml up  --force-recreate app
