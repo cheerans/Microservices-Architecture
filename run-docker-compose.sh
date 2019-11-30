ac07812d58859a3dc023fa6f5d17227b4b7e7b71
Santhosh Cheeran <40557059+cheerans@users.noreply.github.com>
2019-11-30 05:28:41 -0500

    Update run-docker-compose.sh

--- run-docker-compose.sh
+++ run-docker-compose.sh
@@ -1,4 +1,6 @@
-docker-compose -f $PWD//docker-compose-east-framework.yml up msapigateway-east-server1
-docker-compose -f $PWD//docker-compose-east.yml up mlserver-east
+docker-compose -f $PWD//docker-compose-east-framework.yml up
+docker-compose exec msapigateway-east-server1
+docker-compose -f $PWD//docker-compose-east.yml up
+docker-compose exec mlserver-east
 #docker-compose -f $PWD//docker-compose-west-framework.yml up msapigateway-west-server1
 #docker-compose -f $PWD//docker-compose-west.yml up mlserver-west
