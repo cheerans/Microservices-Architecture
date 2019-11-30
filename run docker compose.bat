bf54f12c77ffe149661c36ec5d134ae50553307a
Santhosh Cheeran <40557059+cheerans@users.noreply.github.com>
2019-11-29 22:54:47 -0500

    Update run docker compose.bat

---
+++ run docker compose.bat
@@ -0,0 +1,4 @@
+docker-compose up docker-compose-east-framework.yml
+docker-compose up docker-compose-east.yml
+#docker-compose up docker-compose-west-framework.yml
+#docker-compose up docker-compose-west.yml
