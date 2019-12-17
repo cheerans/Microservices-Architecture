038cebde47b2e9d063bc567d55c7b8045cbc38de
Santhosh Cheeran <40557059+cheerans@users.noreply.github.com>
2019-12-17 11:02:50 -0500

    Update run-docker-compose.sh

--- run-docker-compose.sh
+++ run-docker-compose.sh
@@ -1,11 +1,11 @@
-  1 #!/bin/bash
-  2 
-  3 export COMPOSE_CONVERT_WINDOWS_PATHS=1
-  4 docker network create "ml-cloud-network-east"
-  5 docker volume create "userdata"
-  6 docker-compose -f "$PWD//docker-compose-east-framework.yml" up -d msapigateway-east-server1
-  7 docker-compose -f "$PWD//docker-compose-east.yml" up -d mlserver-east
-  8 #docker network create ml-cloud-network-west
-  9 #docker volume create mlservicedatadir-west
- 10 #docker-compose -f "$PWD//docker-compose-west-framework.yml" up msapigateway-west-server1
- 11 #docker-compose -f "$PWD//docker-compose-west.yml" up mlserver-west
+#!/bin/bash
+
+export COMPOSE_CONVERT_WINDOWS_PATHS=1
+docker network create "ml-cloud-network-east"
+docker volume create "userdata"
+docker-compose -f "$PWD//docker-compose-east-framework.yml" up -d msapigateway-east-server1
+docker-compose -f "$PWD//docker-compose-east.yml" up -d mlserver-east
+#docker network create ml-cloud-network-west
+#docker volume create mlservicedatadir-west
+#docker-compose -f "$PWD//docker-compose-west-framework.yml" up msapigateway-west-server1
+#docker-compose -f "$PWD//docker-compose-west.yml" up mlserver-west
