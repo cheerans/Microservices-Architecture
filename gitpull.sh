a725816153e8cd48cb20a21281c2b47ef4cfb929
Santhosh Cheeran <40557059+cheerans@users.noreply.github.com>
2019-12-17 14:49:31 -0500

    Update gitpull.sh

--- gitpull.sh
+++ gitpull.sh
@@ -4,12 +4,8 @@ git stash
 git pull
 chmod 755 *.sh
 dos2unix *.sh
-cmd='echo -n -e alias git="./gitpull.sh" \r\n'
-echo $cmd
-$cmd
-cmd='echo -n -e alias k="./kill.sh" \r\n'
-echo $cmd
-$cmd
+alias git="./gitpull.sh"
+alias k="./kill.sh"
 cmd="echo -n -e "\x00\x00\x0A\x00        1      012601234${x}12345\x00" | nc -w1 -u localhost 30000"
 echo $cmd
 $cmd
