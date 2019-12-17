d1c795a7eb7b01ba035ec947dfbfa5e6c8c7bd28
Santhosh Cheeran <40557059+cheerans@users.noreply.github.com>
2019-12-17 16:03:41 -0500

    Create rebuild_docker_images.sh

---
+++ rebuild_docker_images.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker build -f MLEurekaServer/docker/DockerFile.eureka -t santhoshcheeran/mleurekaserverrepo .
+docker push santhoshcheeran/mleurekaserverrepo
+docker build -f MLService/docker/DockerFile.mlservice -t santhoshcheeran/mlservicerepo .
+docker push santhoshcheeran/mlservicerepo
+docker build -f MSApiGateway/docker/DockerFile.msapigateway -t santhoshcheeran/msapigatewayrepo .
+docker push santhoshcheeran/msapigatewayrepo
+docker build -f ZipkinServer/docker/DockerFile.zipkinserver -t santhoshcheeran/zipkinserverrepo .
+docker push santhoshcheeran/zipkinserverrepo
