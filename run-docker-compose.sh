d4ee182a8be554a552d51149cc5b56578b096d7c
Santhosh Cheeran <40557059+cheerans@users.noreply.github.com>
2019-12-18 14:12:07 -0500

    Update run-docker-compose.sh

--- run-docker-compose.sh
+++ run-docker-compose.sh
@@ -3,9 +3,8 @@
 export COMPOSE_CONVERT_WINDOWS_PATHS=1
 docker network create "ml-cloud-network-east"
 docker volume create "userdata"
-docker-compose  --compatibility -f "docker-compose.yml" up
-#docker-compose  --compatibility -f "docker-compose-east-framework.yml" up -d msapigateway-east-server1
-#docker-compose  --compatibility -f "docker-compose-east.yml" up -d mlserver-east
+docker-compose  --compatibility -f "docker-compose-east-framework.yml" up -d msapigateway-east-server1
+docker-compose  --compatibility -f "docker-compose-east.yml" up -d mlserver-east
 #docker network create ml-cloud-network-west
 #docker volume create mlservicedatadir-west
 #docker-compose  --compatibility -f "docker-compose-west-framework.yml" up msapigateway-west-server1
