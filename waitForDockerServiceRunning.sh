1769336da87425b39e7462cdf2ee7f1846a55ebf
Santhosh Cheeran <40557059+cheerans@users.noreply.github.com>
2019-12-20 17:12:25 -0500

    Update waitForDockerServiceRunning.sh

--- waitForDockerServiceRunning.sh
+++ waitForDockerServiceRunning.sh
@@ -4,7 +4,7 @@ Container_ID=$1
 echo $Container_ID
 while ! docker inspect -f {{.State.Health.Status}} $Container_ID | grep "healthy" 
 do
-  echo "waiting for " + $Container_ID + " to start ....\r\n"
+  echo "waiting for "$Container_ID" to start ....\r\n"
   sleep 1
 done
 
