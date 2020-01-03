600a8d25f432eaddb390c2c94fff773cd2918ec0
Santhosh Cheeran <40557059+cheerans@users.noreply.github.com>
2020-01-03 15:30:21 -0500

    Update ApiZUULGateway.java

--- MSApiGateway/src/main/java/com/ml/client/ApiZUULGateway.java
+++ MSApiGateway/src/main/java/com/ml/client/ApiZUULGateway.java
@@ -5,19 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
-
-import brave.sampler.Sampler;
-
-@SpringBootApplication
-@EnableDiscoveryClient
-@EnableZuulProxy
-package com.ml.client;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -28,22 +15,6 @@ import brave.sampler.Sampler;
 @EnableZuulProxy
 @EnableWebMvc
 @ComponentScan(basePackages = "com.ml.client")
-public class ApiZUULGateway {
-
-    public static void main(String[] args) {
-    	
-    	System.setProperty("spring.config.name", "ApiZUULGateway");
-        SpringApplication.run(ApiZUULGateway.class, args);        
-        
-        //MLMicSvcClient client = new MLMicSvcClient();
-        //client.callMLService();
-    }
-    
-    @Bean
-    public Sampler defaultSampler() {
-    	return Sampler.ALWAYS_SAMPLE;
-    }
-}
 public class ApiZUULGateway {
 
     public static void main(String[] args) {
