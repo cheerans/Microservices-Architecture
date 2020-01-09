4bee675d53afa7ebd0b57ab35a5bd1eafe3173ad
Santhosh Cheeran <40557059+cheerans@users.noreply.github.com>
2020-01-09 16:32:43 -0500

    Update run-docker-compose.sh

--- run-docker-compose.sh
+++ run-docker-compose.sh
@@ -10,11 +10,14 @@ docker-compose --compatibility -f "docker-compose-east-framework.yml" up -d eure
 docker-compose --compatibility -f "docker-compose-east-framework.yml" up -d eureka-east-server2
 ./waitForDockerServiceRunning.sh  microservices-architecture_eureka-east-server2_1
 
-#docker-compose  --compatibility -f "docker-compose-east-framework.yml" up -d zipkinserver-east-server1
-#./waitForDockerServiceRunning.sh microservices-architecture_zipkinserver-east-server1_1
+docker-compose  --compatibility -f "docker-compose-east-framework.yml" up -d zipkinserver-east-server1
+./waitForDockerServiceRunning.sh microservices-architecture_zipkinserver-east-server1_1
 
 docker-compose  --compatibility -f "docker-compose-east.yml" up -d mlserver-east
 ./waitForDockerServiceRunning.sh microservices-architecture_mlserver-east_1
+
+docker-compose  --compatibility -f "docker-compose-east.yml" up -d mlserver-east
+./waitForDockerServiceRunning.sh microservices-architecture_mlserver-east_2
 
 docker-compose  --compatibility -f "docker-compose-east-framework.yml" up -d msapigateway-east-server1
 ./waitForDockerServiceRunning.sh microservices-architecture_msapigateway-east-server1_1
