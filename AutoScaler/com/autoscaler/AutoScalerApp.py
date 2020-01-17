d08a678ba387a3aba1ac35d35ee84bae9c3a7c2f
Santhosh Cheeran <40557059+cheerans@users.noreply.github.com>
2020-01-17 17:16:39 -0500

    Create AutoScalerApp.py

---
+++ AutoScaler/com/autoscaler/AutoScalerApp.py
@@ -0,0 +1,27 @@
+import logging
+import os
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+from pytz import utc
+
+from com.autoscaler.service.DockerService import DockerService
+import yaml
+
+from com.autoscaler.service.AutoScaleStrategy import AutoScaleStrategy
+
+DEFAULT_LOG_LEVEL = 'info'
+
+logger = logging.getLogger(__name__)
+
+if __name__ == "__main__":
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        level=logging.getLevelName(DEFAULT_LOG_LEVEL.upper()))
+
+    config_filename = os.environ["CONFIG_FILE"]
+    with open(config_filename) as config_file:
+        config = yaml.load(config_file)
+        logger.debug("Config %s", config)
+        dockerSvc = DockerService()
+        scheduler = BlockingScheduler(timezone=utc)
+        autoscaler = AutoScaleStrategy(config, dockerSvc, scheduler)
+        autoscaler.start()
