f568e375db635531fbcbed1dd6f2d43754291722
Santhosh Cheeran <40557059+cheerans@users.noreply.github.com>
2020-01-17 18:58:10 -0500

    Update rebuild_docker_images.sh

--- rebuild_docker_images.sh
+++ rebuild_docker_images.sh
@@ -8,5 +8,7 @@ docker build -f MSApiGateway/docker/DockerFile.msapigateway -t santhoshcheeran/m
 docker push santhoshcheeran/msapigatewayrepo
 docker build -f ZipkinServer/docker/DockerFile.zipkinserver -t santhoshcheeran/zipkinserverrepo .
 docker push santhoshcheeran/zipkinserverrepo
-docker build -f NginxConf/docker/DockerFile.nginx -t santhoshcheeran/nginxrepo .
-docker push santhoshcheeran/nginxrepo
+docker build -f AutoScaler/docker/DockerFile.autoscaler -t santhoshcheeran/autoscalerrepo .
+docker push santhoshcheeran/autoscalerrepo
+#docker build -f NginxConf/docker/DockerFile.nginx -t santhoshcheeran/nginxrepo .
+#docker push santhoshcheeran/nginxrepo
