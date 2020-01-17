9358910c2fa1df1a1d661687cf63a59d034b49d3
Santhosh Cheeran <40557059+cheerans@users.noreply.github.com>
2020-01-17 18:43:55 -0500

    Update DockerService.py

--- AutoScaler/com/service/DockerService.py
+++ AutoScaler/com/service/DockerService.py
@@ -1,3 +1,5 @@
+import json
+
 import docker
 import urllib3
 from docker.types import ServiceMode
@@ -5,21 +7,36 @@ from docker.types import ServiceMode
 from com.autoscaler.exception.ServiceNotFoundException import ServiceNotFoundException
 
 
+def jsons(resp):
+    pass
+
+
 class DockerService(object):
     def __init__(self):
         self.docker_engine = docker.from_env()
 
     def get_req_rate(self,service_name):
-
-        content = urllib3.PoolManager.request('GET','http://localhost:8091/actuator/metrics/req.count')
-        content = content.read()
-        return content
+        req_rate = None
+        resp = urllib3.PoolManager().request('GET','http://localhost:8091/actuator/metrics/req.count')
+        resp = resp.data
+        resp=json.loads(resp)
+        if 'measurements' in resp:
+            resp=next(filter(lambda x: x['statistic'] == 'COUNT', resp['measurements']))
+            if 'value' in resp:
+                req_rate = int(resp['value'])
+        return req_rate
 
     def get_cpu_usage(self,service_name):
 
-        content = urllib3.PoolManager.request('GET','http://localhost:8091/actuator/metrics/req.count')
-        content = content.read()
-        return content
+        req_rate = None
+        resp = urllib3.PoolManager().request('GET','http://localhost:8091/actuator/metrics/req.count')
+        resp = resp.data
+        resp=json.loads(resp)
+        if 'measurements' in resp:
+            resp=next(filter(lambda x: x['statistic'] == 'COUNT', resp['measurements']))
+            if 'value' in resp:
+                req_rate = int(resp['value'])
+        return req_rate
 
     def _get_service(self, service_name):
         services = self.docker_engine.services.list(filters=dict(name=service_name))
