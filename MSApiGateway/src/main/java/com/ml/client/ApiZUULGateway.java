054213fc8875f79f56888f1292684711d9bc31f5
Santhosh Cheeran <40557059+cheerans@users.noreply.github.com>
2020-01-17 20:12:30 -0500

    Update ApiZUULGateway.java

--- MSApiGateway/src/main/java/com/ml/client/ApiZUULGateway.java
+++ MSApiGateway/src/main/java/com/ml/client/ApiZUULGateway.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-//import brave.sampler.Sampler;
+import brave.sampler.Sampler;
 
 @SpringBootApplication
 @EnableDiscoveryClient
