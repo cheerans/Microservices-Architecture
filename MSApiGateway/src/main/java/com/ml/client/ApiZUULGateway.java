f357b1b322d6a9468a9850ee3420ca624c2f49fe
Santhosh Cheeran <40557059+cheerans@users.noreply.github.com>
2020-01-17 20:28:32 -0500

    Update ApiZUULGateway.java

--- MSApiGateway/src/main/java/com/ml/client/ApiZUULGateway.java
+++ MSApiGateway/src/main/java/com/ml/client/ApiZUULGateway.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
